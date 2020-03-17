90462e27a69647ce1d883bc68d745ad2e67108a5
최홍희 <vvshinevv@gmail.com>
2020-03-17 20:29:56 +0900

    [step3]에러나는 것 정리

--- client/src/main/java/sjt/http/client/SjtWebClient.java
+++ client/src/main/java/sjt/http/client/SjtWebClient.java
@@ -1,22 +1,20 @@
 package sjt.http.client;
 
+import com.squareup.okhttp.OkHttpClient;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 
 public class SjtWebClient {
 
     public static void main(String[] args) throws IOException {
-//        jdkClient();
-//        useOkHttpClient();
-        useCloneOkHttpClient();
+        useOkHttpClient();
     }
 
-    private static void useCloneOkHttpClient() throws IOException {
-        sjt.http.client.clone.OkHttpClient client = new sjt.http.client.clone.OkHttpClient();
+    private static void useOkHttpClient() throws IOException {
+        OkHttpClient client = new OkHttpClient();
         HttpURLConnection connection = client.open(new URL("http://localhost:8080/testabc"));
         String contentType = connection.getContentType();
         System.out.println(contentType);
@@ -31,36 +29,6 @@ public class SjtWebClient {
             sb.append(new String(buffer));
         }
         System.out.println(sb.toString());
-    }
-
-    private static void jdkClient() throws IOException {
-        URL url = new URL("http://localhost:8080/testabc");
-        URLConnection urlConnection = url.openConnection();
-        InputStream inputStream = urlConnection.getInputStream();
-        byte[] buffer = new byte[512];
-        StringBuilder sb = new StringBuilder();
-        while (inputStream.read(buffer) != -1) {
-            sb.append(new String(buffer));
-        }
-        System.out.println(sb.toString());
-    }
-
-    private static void useOkHttpClient() throws IOException {
-//        OkHttpClient client = new OkHttpClient();
-//        HttpURLConnection connection = client.open(new URL("http://localhost:8080/testabc"));
-//        String contentType = connection.getContentType();
-//        System.out.println(contentType);
-//        System.out.println(connection.getResponseCode());
-//        System.out.println(connection.getResponseMessage());
-//        System.out.println(connection.getHeaderFields().toString());
-//
-//        InputStream inputStream = connection.getInputStream();
-//        byte[] buffer = new byte[512];
-//        StringBuilder sb = new StringBuilder();
-//        while (inputStream.read(buffer) != -1) {
-//            sb.append(new String(buffer));
-//        }
-//        System.out.println(sb.toString());
     }
 
 }
