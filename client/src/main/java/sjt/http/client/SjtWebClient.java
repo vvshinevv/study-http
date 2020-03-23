d730d1a03c3ebf51892561777abb13f856484ad5
최홍희 <vvshinevv@gmail.com>
2020-03-23 23:12:49 +0900

    [debug-okhttp-vvshinevv]client post로 수정

--- client/src/main/java/sjt/http/client/SjtWebClient.java
+++ client/src/main/java/sjt/http/client/SjtWebClient.java
@@ -4,8 +4,12 @@ import com.squareup.okhttp.OkHttpClient;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class SjtWebClient {
 
@@ -15,15 +19,31 @@ public class SjtWebClient {
 
     private static void useOkHttpClient() throws IOException {
         OkHttpClient client = new OkHttpClient();
+
+        // Request 시작 ======
         HttpURLConnection connection = client.open(new URL("http://localhost:8080/testabc"));
+        connection.setRequestProperty("Content-type", "text/plain");
+        connection.setRequestMethod("POST");
+        OutputStream outputStream = connection.getOutputStream();
+        OutputStreamWriter writer = new OutputStreamWriter(outputStream, "UTF-8");
+        writer.write("outputstream hahahaha");
+        writer.flush();
+        // Request 끝 =======
+
+        // Response 시작 ======
+
+
+
         String contentType = connection.getContentType();
         System.out.println(contentType);
         System.out.println(connection.getResponseCode());
         System.out.println(connection.getResponseMessage());
         System.out.println(connection.getHeaderFields().toString());
 
+
+
         InputStream inputStream = connection.getInputStream();
-        byte[] buffer = new byte[inputStream.available() ];
+        byte[] buffer = new byte[inputStream.available()];
         StringBuilder sb = new StringBuilder();
         int result;
         while ((result = inputStream.read(buffer)) != -1) {
@@ -33,6 +53,7 @@ public class SjtWebClient {
             }
         }
         System.out.println(sb.toString());
+        // Response 끝 ======
     }
 
 }
--- server/src/main/java/sjt/http/client/SjtWebServer.java
+++ server/src/main/java/sjt/http/client/SjtWebServer.java
@@ -3,9 +3,14 @@ package sjt.http.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @SpringBootApplication
@@ -17,8 +22,14 @@ public class SjtWebServer {
     }
 
     @GetMapping("/{message}")
-    public String test(@PathVariable String message) {
+    public String getTest(HttpServletRequest request, @PathVariable String message) {
         return message.toUpperCase();
     }
 
+    @PostMapping(value = "/{message}", consumes = {"text/plain", "application/*"}, produces = {"text/plain", "application/*"})
+    public String postTest(HttpServletRequest request, HttpServletResponse response, @PathVariable String message,
+            @RequestBody String text) {
+        System.out.println(text);
+        return message.toUpperCase();
+    }
 }
